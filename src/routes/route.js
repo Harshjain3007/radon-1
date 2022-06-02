76ba03159605817678c1759ffa9d72070da832ac
Harshjain3007 <hjain3007@gmail.com>
2022-06-02 07:48:45 +0530

    demo4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const externelmodule = require('../routes/route.js')
+//const externelmodule = require('../util/helper')
+//const externelmodule = require('../validator/formater')
+
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
+router.get('/test-me', function(req, res) {
 
-module.exports = router;
-// adding this comment for no reason
+});

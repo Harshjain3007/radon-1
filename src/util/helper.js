f2b38d39015dae6b3f567a79b5e42778ca66952d
Harshjain3007 <hjain3007@gmail.com>
2022-06-02 22:27:50 +0530

    demo4

---
+++ src/util/helper.js
@@ -0,0 +1,17 @@
+
+
+const printDate = function(){
+   const d = new Date()
+    console.log(d.getMonth())
+    
+}
+module.exports.printDate = printDate
+
+const printMonth = function(){
+      console.log("June")
+}
+module.exports.printMonth = printMonth
+const  getBatchInfo = function(){
+console.log("Radon W3, the topic been taught today is nodejs module system")
+}
+module.exports.getBatchInfo = getBatchInfo

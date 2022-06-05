79dac636b7972ec11d971aa58497a663a9f1bfcd
Harshjain3007 <hjain3007@gmail.com>
2022-06-05 13:12:04 +0530

    new4

--- src/routes/route.js
+++ src/routes/route.js
@@ -35,7 +35,65 @@ router.get('/candidates/:canidatesName', function(req, res){
     console.log('Candidates name is '+req.params.canidatesName)
     res.send('Done')
 })
+//1>>>>>>>>>>>>>>>>>>>>>>>>.
+router.get('/movies',function(req,res){
+    const movies = ['Rang De Basanti', 'The shining','Lord of the rings', 'Batman begins'] 
+    res.send(movies)
+
+})
+
+//2 and 3 >>>>>>>>>>>>
+router.get('/movies/:indexNumber', function(req,res){
+let movies = ['Rang De Basanti', 'The shining','Lord of the rings', 'Batman begins'] 
+if(req.params.indexnumber<4){
+    console.log(movies[req.params.indexnumber])
+res.send(movies[req.params.indexnumber])}
+else{
+console.log('invalid movie not availabe')
+res.send(movies['invalid movie not available'])
+}
+})
+//4
+router.get('/films', function(req,res){
+let movies=    [ {
+                "id":1,
+                 "name":"Rang de Basanti"},
+                 {
+                     "id":2,
+                     "name":"Incendies",
+                  },
+
+                  {
+                      "id3":3,
+                      "name": "The shining",
+
+                 },
+                 {
+                     "id":4,
+                     "name":"Finding nemo",
+                 }
+
+    
+             ]
+       res.send(movies)
+})
+
+
+
+
+
+
+
 
 
 module.exports = router;
 // adding this comment for no reason
+
+
+
+
+
+
+
+
+

73a83718cf1d343432014340c9220fbaede7b027
Harshjain3007 <hjain3007@gmail.com>
2022-06-16 15:42:33 +0530

    asdd2

--- src/Middlewares/Mid.js
+++ src/Middlewares/Mid.js
@@ -1,4 +1,4 @@
-const jwt = require("jasonwebtoken");
+const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-
+const Mid =  require("../Middlewares/Mid")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -11,9 +11,9 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",Mid.authentication,userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete("/users/:userId", userController.removeuser)
+router.put("/users/:userId",Mid.authentication,userController.updateUser)
+router.delete("/users/:userId",Mid.authentication, userController.removeuser)
 
 module.exports = router;
